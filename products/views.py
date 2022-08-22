0719b3e46c26e84242737d1edd6ed2f8c0f76e68
Jiang <jiangyue0380@gmail.com>
2022-08-22 23:56:24 +1000

    updated product_list function in views.py that gets all the products from the database

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.models import Product
+from products.serilizer import ProductSerializer
 
 
 @api_view(['GET'])
 def product_list(request):
-    return Response('GET request works!')
+    prodcuts = Product.objects.all()
+    serilizers = ProductSerializer(prodcuts, many=True)
+    return Response(serilizers.data)
