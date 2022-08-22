e8699d1bcf27631154974ae6b87b96c57c17be4c
Jiang <jiangyue0380@gmail.com>
2022-08-23 00:32:47 +1000

    updated product_list function for POST request to create a product to add to the database in views.py

--- products/views.py
+++ products/views.py
@@ -1,19 +1,27 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import status
 from products.models import Product
 from products.serilizer import ProductSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
-    prodcuts = Product.objects.all()
-    serilizers = ProductSerializer(prodcuts, many=True)
-    return Response(serilizers.data)
+    if request.method == 'GET':
+        prodcuts = Product.objects.all()
+        serilizers = ProductSerializer(prodcuts, many=True)
+        return Response(serilizers.data)
+    elif request.method == 'POST':
+        serilizers = ProductSerializer(data=request.data)
+        if serilizers.is_valid() == True:
+            serilizers.save()
+            return Response(serilizers.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serilizers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
-def product_details(requst, pk):
+def product_details(request, pk):
     try:
         product = Product.objects.get(pk=pk)
         serilizers = ProductSerializer(product)
