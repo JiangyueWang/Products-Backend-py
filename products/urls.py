b60252dfe34eb8e09ae7ea84530bcde2acfe3f01
Jiang <jiangyue0380@gmail.com>
2022-08-23 00:10:25 +1000

    updated products app urls.py + created product_details function in views.py to get details of a specific product

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('', views.product_list)
+    path('', views.product_list),
+    path('<int:pk>', views.product_details),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from products.models import Product
 from products.serilizer import ProductSerializer
 
@@ -9,3 +10,13 @@ def product_list(request):
     prodcuts = Product.objects.all()
     serilizers = ProductSerializer(prodcuts, many=True)
     return Response(serilizers.data)
+
+
+@api_view(['GET'])
+def product_details(requst, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serilizers = ProductSerializer(product)
+        return Response(serilizers.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
