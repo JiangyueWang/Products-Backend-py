a1e097079e608925f87b907082c20b5cbc3b98a2
Jiang <jiangyue0380@gmail.com>
2022-08-24 22:17:26 +1000

    updated serializer.py and serializer variables in views.py

--- products/views.py
+++ products/views.py
@@ -3,22 +3,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from products.models import Product
-from products.serilizer import ProductSerializer
+from .serializer import ProductSerializer
 
 
 @api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == 'GET':
         prodcuts = Product.objects.all()
-        serilizers = ProductSerializer(prodcuts, many=True)
-        return Response(serilizers.data)
+        serializer = ProductSerializer(prodcuts, many=True)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        serilizers = ProductSerializer(data=request.data)
-        if serilizers.is_valid() == True:
-            serilizers.save()
-            return Response(serilizers.data, status=status.HTTP_201_CREATED)
+        serializer  = ProductSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serilizers.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -26,15 +26,15 @@ def product_details(request, pk):
     product = Product.objects.get(pk=pk)
     if request.method == 'GET':
         try:
-            serilizers = ProductSerializer(product)
-            return Response(serilizers.data)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
         except Product.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
     elif request.method == 'PUT':
-        serilizers = ProductSerializer(product, data=request.data)
-        if serilizers.is_valid() == True:
-            serilizers.save()
-            return Response(serilizers.data, status=status.HTTP_200_OK)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
@@ -45,10 +45,10 @@ def product_details(request, pk):
 @api_view(['PATCH'])
 def update_product_url(request, pk):
     product = Product.objects.get(pk=pk)
-    serilizers = ProductSerializer(product, data=request.data, partial=True)
-    if serilizers.is_valid() == True:
-        serilizers.save()
-        return Response(serilizers.data, status=status.HTTP_200_OK)
+    serializer = ProductSerializer(product, data=request.data, partial=True)
+    if serializer.is_valid() == True:
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
